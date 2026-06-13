b5d2fb8c6f63b18483ec6ac8fd8c0a6ac9439eb8
Ankush kumar <ankushx970@gmail.com>
2026-06-13 16:49:25 +0530

    chore: setup client (Turbo repo + Tanstack Start)

---
+++ client/apps/web/prettier.config.js
@@ -0,0 +1,8 @@
+import sharedConfig from '@repo/prettier-config'
+
+/** @type {import('prettier').Config} */
+const config = {
+  ...sharedConfig,
+}
+
+export default config
---
+++ client/packages/ui/src/index.ts
@@ -0,0 +1,3 @@
+export { Header } from "./components/Header";
+export { Footer } from "./components/Footer";
+export { ThemeToggle } from "./components/ThemeToggle";
