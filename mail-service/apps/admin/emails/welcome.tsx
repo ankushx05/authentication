b7ddb40502305edd6133027f6bd6933cb82a3ee8
Ankush kumar <ankushx970@gmail.com>
2026-06-24 17:22:09 +0530

    feat: introduce @repo/templates workspace package for shared email templates

--- mail-service/apps/admin/emails/welcome.tsx
+++ mail-service/apps/admin/emails/welcome.tsx
@@ -1,38 +1,5 @@
-import {
-  Body,
-  Container,
-  Head,
-  Heading,
-  Html,
-  Preview,
-  Text,
-} from "@react-email/components";
+import { WelcomeEmail as WelcomeEmailTemplate } from "@repo/templates/admin";
 
 export default function WelcomeEmail() {
-  return (
-    <Html>
-      <Head />
-      <Preview>Welcome to our platform!</Preview>
-      <Body
-        style={{
-          fontFamily: "sans-serif",
-          backgroundColor: "#f6f9fc",
-          padding: "20px",
-        }}
-      >
-        <Container
-          style={{
-            backgroundColor: "#ffffff",
-            padding: "40px",
-            borderRadius: "8px",
-          }}
-        >
-          <Heading style={{ color: "#333333" }}>Welcome to App admin!</Heading>
-          <Text style={{ color: "#555555" }}>
-            Thank you for signing up. We're excited to have you on board!
-          </Text>
-        </Container>
-      </Body>
-    </Html>
-  );
+  return <WelcomeEmailTemplate />;
 }
---
+++ mail-service/packages/templates/src/web/welcome.tsx
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Text,
+} from "@react-email/components";
+
+export const WelcomeEmail = () => {
+  return (
+    <Html>
+      <Head />
+      <Preview>Welcome to our platform!</Preview>
+      <Body
+        style={{
+          fontFamily: "sans-serif",
+          backgroundColor: "#f6f9fc",
+          padding: "20px",
+        }}
+      >
+        <Container
+          style={{
+            backgroundColor: "#ffffff",
+            padding: "40px",
+            borderRadius: "8px",
+          }}
+        >
+          <Heading style={{ color: "#333333" }}>Welcome to App web!</Heading>
+          <Text style={{ color: "#555555" }}>
+            Thank you for signing up. We're excited to have you on board!
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
